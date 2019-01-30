0c70f60345bf1c91acb1b9be2650b728a5f8b364
Thomas Morgan <tamorgan@outlook.com>
2019-01-30 19:04:04 +0000

    Random movements are now normally distributed

--- crate/src/state.rs
+++ crate/src/state.rs
@@ -1,29 +1,32 @@
 use crate::particle::Particle;
 use rand::prelude::ThreadRng;
-use rand::Rng;
+use rand::distributions::{Normal, Distribution};
+use std::time::{Duration, Instant};
 
 pub struct State {
     particles: Vec<Particle>,
     rng: ThreadRng,
     max_bounds: [f64; 2],
+    normal_distribution: Normal,
 }
 
 impl State {
     pub fn new(width: f64, height: f64) -> State {
         let mut particles = Vec::new();
         let rng = rand::thread_rng();
+        let normal_distribution = Normal::new(0.0, 3.0);
 
-        for _ in 1..10000 {
+        for _ in 1..1000 {
             particles.push(Particle::new([width / 2.0, height / 2.0]));
         }
 
-        State { particles, rng, max_bounds: [width, height] }
+        State { particles, rng, max_bounds: [width, height], normal_distribution: normal_distribution }
     }
 
     pub fn tick(&mut self) {
         for particle in self.particles.iter_mut() {
             for i in 0..2 {
-                let new_position = particle.pos()[i] + self.rng.gen_range(-10.0, 10.0);
+                let new_position = particle.pos()[i] + self.normal_distribution.sample(&mut self.rng);
 
                 if new_position > 0.0 && new_position < self.max_bounds[i] {
                     particle.pos_mut()[i] = new_position;
